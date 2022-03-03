a1e48ec8c46a78f0e5554f9df68e240ceee375a7
Konstructa <elenprojects7@gmail.com>
2022-03-03 15:33:11 -0300

    add view engine e port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 app.use(express.static('src'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/src/index.html')
 })
 
-app.listen(process.env.PORT || 3000); 
+app.listen(process.env.PORT || 3000)
